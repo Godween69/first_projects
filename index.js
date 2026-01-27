32500bcbc779c8b0103c622deb73ff2f714ad4b7
Godween69 <tver69rus@gmail.com>
2026-01-28 00:05:49 +0300

    Добавил HTML, CSS и JS

---
+++ index.js
@@ -0,0 +1,7 @@
+
+var x = document.getElementById("button_1");
+
+function myFunction() {
+  x.style.cssText = "background-color: #f36507; padding: 10px 25px";
+}
+x.addEventListener("click", myFunction);  
